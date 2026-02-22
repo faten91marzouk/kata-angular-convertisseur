75f4c12d0cb836c4b379ea6b2aba22cb034bfd23
Faten Marzouk <faten91.marzouk@gmail.com>
2026-02-22 21:04:06 +0100

    chore: initial project setup - skeleton

---
+++ src/app/component/converter/converter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-converter',
+  imports: [],
+  templateUrl: './converter.html',
+  styleUrl: './converter.scss',
+})
+export class Converter {
+
+}
