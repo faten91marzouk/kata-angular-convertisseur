51e69510c7150a646a4a60a7b7590c5961bad043
Faten Marzouk <faten91.marzouk@gmail.com>
2026-02-22 21:18:50 +0100

    feat: add exchange store service + unit TDD tests

--- src/app/store/exchange.spec.ts
+++ src/app/store/exchange.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { Exchange } from './exchange';
 
 describe('Exchange', () => {
@@ -13,4 +12,25 @@ describe('Exchange', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+   it('should initialize rate to 1.1', () => {
+    expect(service.rate()).toBe(1.1);
+  });
+
+  it('should convert EUR to USD correctly', () => {
+  service.amount.set(10);
+  service.currency.set('EUR');
+});
+
+it('should add a conversion to history', () => {
+  service.amount.set(10);
+  service.currency.set('EUR');
+  expect(service.history().length).toBe(1);
+
+  const entry = service.history()[0];
+
+  expect(entry.inputValue).toBe(10);
+  expect(entry.inputCurrency).toBe('EUR');
+  expect(entry.outputValue).toBe(11); // 10 * 1.1
+  expect(entry.outputCurrency).toBe('USD');
+});
 });
--- src/app/store/exchange.ts
+++ src/app/store/exchange.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, computed, effect } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Exchange {
+
+  rate = signal<number>(1.1);              
+  fixedRate = signal<number | null>(null); 
+  amount = signal<number>(0);              
+  currency = signal<'EUR' | 'USD'>('EUR'); 
+  history = signal<any[]>([]);        
   
 }
